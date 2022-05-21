3c89857839505f4630acb50c6529cda2cc058a14
tincon <tincon1mc@gmail.com>
2022-05-20 21:00:57 -0500

    update

--- src/Home.tsx
+++ src/Home.tsx
@@ -592,7 +592,7 @@ const Home = (props: HomeProps) => {
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
-                                src="ganja-gif.gif"
+                                src="cool-cats.gif"
                                 alt="NFT To Mint"/></div>
                             <br/>
                             {wallet && isActive && whitelistEnabled && (whitelistTokenBalance > 0) && isBurnToken &&
